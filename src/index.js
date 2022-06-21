41393413d7537aed616c0fda826ba2455e755622
Abdulozy <abdulozy007@gmail.com>
2022-06-21 15:33:46 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // initialize taskList class
-  const taskList = new TaskList();
-  //grab all the necessary DOM elements
-
-  //form and relevant input fields
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-
-  //ul where new tasks will live on the DOM
-  const taskUl = document.getElementById("tasks");
-
-  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
-  //attach event listeners
-
-  newTaskForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    taskList.createNewTask(newTaskDescription.value);
-    // reset form
-    e.target.reset();
-    renderApp();
-  });
-
-  taskUl.addEventListener("click", (e) => {
-    if (e.target.nodeName === "BUTTON") {
-      taskList.deleteTask(e.target.dataset.description);
-      renderApp();
-    }
-  });
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = document.getElementById("new-task-description").value
+    const li = document.createElement("li")
+    li.innerHTML = input
+    document.querySelector("#tasks").appendChild(li)
+    document.querySelector('form').reset()
+  })
 });
